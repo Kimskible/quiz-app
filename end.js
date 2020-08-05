ae0b540216a69e88a877ef81295ce2a3b13a7732
Kimskible <k_oksana15@hotmail.com>
2020-08-04 21:34:49 -0500

    fixed error

--- end.js
+++ end.js
@@ -1,13 +1,30 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
-finalScore.innerText = mostRecentScore;
+const username = document.getElementById('username')
+const saveScoreBtn = document.getElementById('saveScoreBtn')
 
-username.addEventListener('keyup', () => {
+
+username.addEventListener('keyup', () =>{
+    // if nothing inside the usrname field, set button to disable but if there is then enable the button 
     saveScoreBtn.disabled = !username.value;
-});
+})
 
 saveHighScore = (e) => {
     e.preventDefault();
 };
+
+
+
+
+// const username = document.getElementById('username');
+// const saveScoreBtn = document.getElementById('saveScoreBtn');
+// const finalScore = document.getElementById('finalScore');
+// const mostRecentScore = localStorage.getItem('mostRecentScore');
+// finalScore.innerText = mostRecentScore;
+
+// username.addEventListener('keyup', () => {
+//     saveScoreBtn.disabled = !username.value;
+// });
+
+// saveHighScore = (e) => {
+//     e.preventDefault();
+// };
+
